de6c76388325f0cefa63963b08c72490146d7d0f
thomasd7021 <114589769+thomasd7021@users.noreply.github.com>
2024-04-30 23:20:02 +0000

    Tue, Apr 30, 2024, 7:20 PM -04:00

--- tool/mygame/src/main.js
+++ tool/mygame/src/main.js
@@ -144,6 +144,23 @@ const scenes = {
 			enemy.enterState("move")
 		}
 
+		function patrol(speed = 60, dir = 1) {
+			return {
+				id: "patrol",
+				require: [ "pos", "area" ],
+				add() {
+					this.on("collide", (obj, col) => {
+						if (col.isLeft() || col.isRight()) {
+							dir = -dir
+						}
+					})
+				},
+				update() {
+					this.move(speed * dir, 0)
+				},
+			}
+		}
+
 
 		//player interactions
 		player.onCollide("plastic", (coin) => {
